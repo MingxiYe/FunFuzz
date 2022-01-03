967fae6a4ce3caeace88e39f1e2ef5a2c99eebef
MingxiYe <yemx6@mail2.sysu.edu.cn>
2022-01-03 16:56:20 +0800

    feat: add criticalTracebits & criticalPredicates to store critical path

--- libfuzzer/TargetContainerResult.cpp
+++ libfuzzer/TargetContainerResult.cpp
@@ -5,12 +5,16 @@ namespace fuzzer {
   TargetContainerResult::TargetContainerResult(
     unordered_set<string> tracebits,
     unordered_map<string, u256> predicates,
+    unordered_set<string> criticalTracebits,
+    unordered_map<string, u256> criticalPredicates,
     unordered_set<string> uniqExceptions,
     string cksum
   ) {
     this->tracebits = tracebits;
     this->cksum = cksum;
     this->predicates = predicates;
+    this->criticalTracebits = criticalTracebits;
+    this->criticalPredicates = criticalPredicates;
     this->uniqExceptions = uniqExceptions;
   }
 }
--- libfuzzer/TargetContainerResult.h
+++ libfuzzer/TargetContainerResult.h
@@ -13,6 +13,8 @@ namespace fuzzer {
     TargetContainerResult(
         unordered_set<string> tracebits,
         unordered_map<string, u256> predicates,
+        unordered_set<string> criticalTracebits,
+        unordered_map<string, u256> criticalPredicates,
         unordered_set<string> uniqExceptions,
         string cksum
     );
@@ -21,6 +23,10 @@ namespace fuzzer {
     unordered_set<string> tracebits;
     /* Save predicates */
     unordered_map<string, u256> predicates;
+    /* Contains critical execution paths */
+    unordered_set<string> criticalTracebits;
+    /* Save critical predicates */
+    unordered_map<string, u256> criticalPredicates;
     /* Exception path */
     unordered_set<string> uniqExceptions;
     /* Contains checksum of tracebits */
