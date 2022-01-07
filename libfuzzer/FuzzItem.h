2104e03aa70f227cb40907487b2e8e59988a9276
MingxiYe <yemx6@mail2.sysu.edu.cn>
2022-01-07 19:09:25 +0800

    feat: add attribute to record real length of data

--- libfuzzer/FuzzItem.h
+++ libfuzzer/FuzzItem.h
@@ -9,11 +9,14 @@ using namespace eth;
 namespace fuzzer {
   struct FuzzItem {
     bytes data;
+    /* record the real length of each paramater in data for adaptive mutation */
+    vector<pair<int, bool>> realLen;
     TargetContainerResult res;
     uint64_t fuzzedCount = 0;
     uint64_t depth = 0;
-    FuzzItem(bytes _data) {
+    FuzzItem(bytes _data, vector<pair<int, bool>> _realLen) {
       data = _data;
+      realLen = _realLen;
     }
   };
   using OnMutateFunc = function<FuzzItem (bytes b)>;
